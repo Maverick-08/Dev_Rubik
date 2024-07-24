c8f43695df06d2e2b84fd8ff69374b0a4b2455b4
Manishka Gupta <manishkagupta2808@gmail.com>
2024-07-24 15:29:22 +0530

    color palette

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryRed:"#A41045",
+        vividViolet:"#892066",
+        tertiaryPink:"#E52996",
+        tertiaryBlue:"#18457C",
+        primaryDark:"#0A0C10",
+        lightDark:"#424345",
+        "darkGrey":"#7D7E80",
+        "blueGrey":"#95A1AD",
+        "pinkGrey":"#9B8595",
+        "grey":"#B9B9B9",
+        "lightGrey":"#D8D8D8"
+        },
+    },
   },
   plugins: [],
 }
